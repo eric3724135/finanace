c49325bd3de200ac9f2c2ac52a9d0d1fa2e8c2cc
eric <eric3724135@gmail.com>
2024-02-15 15:18:26 +0800

    fix bug

--- src/main/java/com/eric/service/AnalysisService.java
+++ src/main/java/com/eric/service/AnalysisService.java
@@ -2,6 +2,7 @@ package com.eric.service;
 
 import com.eric.domain.Quote;
 import com.eric.domain.Symbol;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeriesBuilder;
@@ -13,13 +14,21 @@ import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class AnalysisService {
 
     public List<Quote> handleRSI(Symbol symbol, List<Quote> quotes, int rsiValue) {
         BarSeries series = new BaseBarSeriesBuilder().withName(symbol.getId()).build();
         Collections.reverse(quotes);
-        quotes.forEach(cmQuote -> series.addBar(ZonedDateTime.of(cmQuote.getTradeDate().atStartOfDay(), ZoneId.systemDefault()), cmQuote.getOpen(), cmQuote.getHigh(), cmQuote.getLow(), cmQuote.getClose(), cmQuote.getVolume()));
+        quotes.forEach(cmQuote -> {
+            try {
+                series.addBar(ZonedDateTime.of(cmQuote.getTradeDate().atStartOfDay(), ZoneId.systemDefault()), cmQuote.getOpen(), cmQuote.getHigh(), cmQuote.getLow(), cmQuote.getClose(), cmQuote.getVolume());
+            } catch (Exception e) {
+                log.warn("[{}] error ", cmQuote.getSymbol(), e);
+
+            }
+        });
         ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
         RSIIndicator rsiIndicator = new RSIIndicator(closePrice, rsiValue);
         int limit = quotes.size() < 31 ? 0 : quotes.size() - 31;
