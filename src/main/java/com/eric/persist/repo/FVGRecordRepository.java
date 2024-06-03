b255162b4f427869238b5aa982fd3ad2f16a2051
eric3724135 <eric3724135@gmail.com>
2024-06-04 00:11:06 +0800

    fvg update

--- src/main/java/com/eric/persist/repo/FVGRecordRepository.java
+++ src/main/java/com/eric/persist/repo/FVGRecordRepository.java
@@ -2,6 +2,7 @@ package com.eric.persist.repo;
 
 import com.eric.persist.pojo.FVGRecordDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
@@ -12,11 +13,12 @@ public interface FVGRecordRepository extends JpaRepository<FVGRecordDto, String>
 
     @Query(value = "SELECT * from fvg_record where id = ?1 and trade_date = ?2 ", nativeQuery = true)
     List<FVGRecordDto> findByIdAndTradeDate(String id, LocalDate date);
+
     @Query(value = "SELECT * from fvg_record where position = ?1 and trade_date between ?2 and ?3 order by trade_date desc", nativeQuery = true)
     List<FVGRecordDto> findRecordByPositionAndRange(String position, LocalDate startDate, LocalDate endDate);
 
     @Query(value = "SELECT * from fvg_record where trade_date between ?1 and ?2 order by trade_date desc", nativeQuery = true)
-    List<FVGRecordDto> findRecordByRange( LocalDate startDate, LocalDate endDate);
+    List<FVGRecordDto> findRecordByRange(LocalDate startDate, LocalDate endDate);
 
 
 }
--- src/main/java/com/eric/service/FVGService.java
+++ src/main/java/com/eric/service/FVGService.java
@@ -116,12 +116,13 @@ public class FVGService {
         List<FavoriteSymbolDto> tweList = symbolService.getFavoriteSymbols(SymbolType.TWE);
         List<FavoriteSymbolDto> usList = symbolService.getFavoriteSymbols(SymbolType.US);
 
+
         for (FavoriteSymbolDto favoriteSymbolDto : tweList) {
             Symbol symbol = Symbol.ofTW(favoriteSymbolDto.getId(), favoriteSymbolDto.getName());
-            List<Quote> quotes = quoteService.getusQuotesFromSite(symbol, "1d", "6mo");
+            List<Quote> quotes = quoteService.getusQuotesFromSite(symbol, "1d", "10y");
             if (quotes == null || quotes.isEmpty()) {
                 symbol = Symbol.ofTWO(favoriteSymbolDto.getId(), favoriteSymbolDto.getName());
-                quotes = quoteService.getusQuotesFromSite(symbol, "1d", "6mo");
+                quotes = quoteService.getusQuotesFromSite(symbol, "1d", "10y");
             }
             if (quotes == null || quotes.size() < 10) {
                 continue;
@@ -129,56 +130,66 @@ public class FVGService {
             List<FVGResult> results = strategy.execute(symbol.getId(), quotes);
 
 //            FVGResult result = results.get(results.size() - 1);
-            for (int i = results.size() - 10; i < results.size() - 1; i++) {
+            for (int i = results.size() - 10; i < results.size(); i++) {
                 FVGResult result = results.get(i);
 
                 if (FVGPosition.BUY.equals(result.getPosition()) || FVGPosition.SELL.equals(result.getPosition())) {
-                    //寫入紀錄
+
                     List<FVGRecordDto> records = fvgRecordRepository.findByIdAndTradeDate(symbol.getId(), result.getTradeDate());
+                    if (!records.isEmpty()) {
+                        if (result.getTradeDate().isEqual(LocalDate.now())) {
+                            fvgRecordRepository.delete(records.get(0));
+                            this.saveFvgRecord(symbol, favoriteSymbolDto, result);
+                        }
+                    }
+                    //寫入紀錄
                     if (records.isEmpty()) {
-                        FVGRecordDto record = new FVGRecordDto();
-                        record.setId(symbol.getId());
-                        record.setName(favoriteSymbolDto.getName());
-                        record.setTradeDate(result.getTradeDate());
-                        record.setClose(result.getClose());
-                        record.setUpAvg(result.getUpAvgValue());
-                        record.setDownAvg(record.getDownAvg());
-                        record.setPosition(result.getPosition().name());
-                        fvgRecordRepository.save(record);
+                        this.saveFvgRecord(symbol, favoriteSymbolDto, result);
                     }
                 }
             }
         }
         for (FavoriteSymbolDto favoriteSymbolDto : usList) {
             Symbol symbol = favoriteSymbolDto.getSymbolObj();
-            List<Quote> quotes = quoteService.getusQuotesFromSite(symbol, "1d", "6mo");
+            List<Quote> quotes = quoteService.getusQuotesFromSite(symbol, "1d", "10y");
             if (quotes == null || quotes.size() < 10) {
                 continue;
             }
             List<FVGResult> results = strategy.execute(symbol.getId(), quotes);
 
 //            FVGResult result = results.get(results.size() - 1);
-            for (int i = results.size() - 10; i < results.size() - 1; i++) {
+            for (int i = results.size() - 10; i < results.size(); i++) {
                 FVGResult result = results.get(i);
 
                 if (FVGPosition.BUY.equals(result.getPosition()) || FVGPosition.SELL.equals(result.getPosition())) {
-                    //寫入紀錄
+
                     List<FVGRecordDto> records = fvgRecordRepository.findByIdAndTradeDate(symbol.getId(), result.getTradeDate());
+                    if (!records.isEmpty()) {
+                        if (result.getTradeDate().isEqual(LocalDate.now())) {
+                            fvgRecordRepository.delete(records.get(0));
+                            this.saveFvgRecord(symbol, favoriteSymbolDto, result);
+                        }
+                    }
+                    //寫入紀錄
                     if (records.isEmpty()) {
-                        FVGRecordDto record = new FVGRecordDto();
-                        record.setId(symbol.getId());
-                        record.setName(favoriteSymbolDto.getName());
-                        record.setTradeDate(result.getTradeDate());
-                        record.setClose(result.getClose());
-                        record.setUpAvg(result.getUpAvgValue());
-                        record.setDownAvg(record.getDownAvg());
-                        record.setPosition(result.getPosition().name());
-                        fvgRecordRepository.save(record);
+                        this.saveFvgRecord(symbol, favoriteSymbolDto, result);
                     }
                 }
             }
         }
 
+    }
+
+    private void saveFvgRecord(Symbol symbol, FavoriteSymbolDto favoriteSymbolDto, FVGResult result) {
+        FVGRecordDto record = new FVGRecordDto();
+        record.setId(symbol.getId());
+        record.setName(favoriteSymbolDto.getName());
+        record.setTradeDate(result.getTradeDate());
+        record.setClose(result.getClose());
+        record.setUpAvg(result.getUpAvgValue());
+        record.setDownAvg(record.getDownAvg());
+        record.setPosition(result.getPosition().name());
+        fvgRecordRepository.save(record);
     }
 
 }
